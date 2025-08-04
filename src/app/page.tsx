39aa30973019e64e299743212ef0cabe7c0651cd
Denis Politkovskiy <d.politkovskiy@alinghi.ru>
2025-08-04 23:29:44 +0400

    fix: for build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -126,7 +126,7 @@ export default function Home() {
             </div>
             <div>
               <label htmlFor="message" className="sr-only">Сообщение</label>
-              <textarea id="message" rows="4" placeholder="Ваше сообщение..." className="mt-1 block w-full border border-gray-300 rounded-lg p-4 outline-none focus:ring-2 focus:ring-blue-500 transition-colors" required></textarea>
+              <textarea id="message" rows={4} placeholder="Ваше сообщение..." className="mt-1 block w-full border border-gray-300 rounded-lg p-4 outline-none focus:ring-2 focus:ring-blue-500 transition-colors" required></textarea>
             </div>
             <button type="submit" className="w-full bg-gradient-to-r from-blue-500 to-indigo-600 text-white p-4 rounded-full font-bold shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-300 flex justify-center items-center space-x-2">
               <span>Отправить заявку</span>
