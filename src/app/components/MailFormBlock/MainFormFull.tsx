52d91b1dd05e4384ac961af3435324bdaa45d559
Denis Politkovskiy <d.politkovskiy@alinghi.ru>
2025-10-13 22:44:29 +0400

    fix: mail

--- src/app/components/MailFormBlock/MainFormFull.tsx
+++ src/app/components/MailFormBlock/MainFormFull.tsx
@@ -1,7 +1,7 @@
 import { EnvelopeIcon, PaperAirplaneIcon, UserIcon } from '@heroicons/react/24/solid'
 import axios from 'axios'
 import { m } from 'framer-motion'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const MainFormFull = () => {
 
@@ -9,11 +9,10 @@ export const MainFormFull = () => {
 	const [mail, setMail] = useState<string>()
 	const [comt, setcomt] = useState<string>()
 
-	const sendEmail = () => {
-		setMail("")
-		setName("")
-		setcomt("")
+	const [showAnswer, setShowAnswer] = useState<boolean>(true)
 
+	const sendEmail = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
 		let form = new FormData()
 
 		if(!mail || !name || !comt){
@@ -23,14 +22,21 @@ export const MainFormFull = () => {
 		form.append("email",mail)
 		form.append("fio",name)
 		form.append("message",comt)
+		setShowAnswer(true)
 
-		axios.post(`http://businessimport.ru:8080/api/send-mail`,form)
+		axios.post(`http://businessimport.ru:8080/api/send-mail`,form).then(()=>setShowAnswer(true))
 	}
+
+	useEffect(() => {
+		let timer = setTimeout(()=>{
+			setShowAnswer(false)},5000)
+		return ()=>{clearTimeout(timer)}
+	},[showAnswer])
 
 	return <section id="contact-form" className="pb-20 bg-gray-100 flex justify-center">
 		<div className="container p-8 py-20 flex flex-col justify-center">
 			<h2 className="text-4xl font-bold text-center text-indigo-900 mb-12">Оставить заявку</h2>
-			<form className="max-w-lg mx-auto bg-white p-10 rounded-xl shadow-2xl space-y-6 w-full md:min-w-[600px]">
+			<form className="max-w-lg mx-auto bg-white p-10 rounded-xl shadow-2xl space-y-6 w-full md:min-w-[600px]" onSubmit={sendEmail}>
 				<div className="flex items-center space-x-4 border-b border-gray-200 py-2">
 					<UserIcon className="h-6 w-6 text-gray-400" />
 					<input value={name} onChange={(e)=>setName(e.target.value)} type="text" id="name" placeholder="Ваше имя" className="flex-1 block w-full outline-none text-gray-700" required />
@@ -43,10 +49,14 @@ export const MainFormFull = () => {
 					<label htmlFor="message" className="sr-only">Сообщение</label>
 					<textarea value={comt} onChange={(e)=>setcomt(e.target.value)} id="message" rows={4} placeholder="Ваше сообщение..." className="mt-1 block w-full border border-gray-300 rounded-lg p-4 outline-none focus:ring-2 focus:ring-blue-500 transition-colors" required></textarea>
 				</div>
-				<button onClick={sendEmail} type="submit" className="w-full bg-gradient-to-r from-blue-500 to-indigo-600 text-white p-4 rounded-full font-bold shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-300 flex justify-center items-center space-x-2">
+				<button type="submit" className="w-full bg-gradient-to-r from-blue-500 to-indigo-600 text-white p-4 rounded-full font-bold shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-300 flex justify-center items-center space-x-2">
 					<span>Отправить заявку</span>
 					<PaperAirplaneIcon className="h-5 w-5" />
 				</button>
+				{showAnswer && <>
+					<h4 className='text-green-700 font-bold text-center'>Ваша заявка принята!</h4>
+					<h4 className='text-green-700 font-bold text-center'>Ожидайте ответа от консультанта</h4>
+				</>}
 			</form>
 		</div>
 	</section>
